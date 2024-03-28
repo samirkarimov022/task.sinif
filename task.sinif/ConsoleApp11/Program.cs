2943bdb1ec5643b39657da45945dc1d9a89a2afc
KamranTalis <az.kamik.rus@gmail.com>
2024-03-28 12:21:43 +0400

    add content

---
+++ task.sinif/ConsoleApp11/Program.cs
@@ -0,0 +1,21 @@
+﻿using ConsoleApp11.Task;
+using System.Drawing;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+
+namespace ConsoleApp11
+{
+	internal class Program
+	{
+		public static void Main()
+		{
+			Student samir = new Student("samir","kerimov","Ab106",74);
+
+
+
+			
+		
+			
+		}
+	}
+}
---
+++ task.sinif/ConsoleApp11/Task/Student.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11.Task
+{
+	public class Student
+	{
+		public string Name;
+		public string SurName;
+		public string Group;
+		public decimal Point;
+		public bool IsGraduate = true;
+
+		public void Task(string name, string surnama, string group, decimal point)
+		{
+			Name= name;
+			SurName= surnama;
+			Group= group;
+			Point= point;
+			
+		}
+		public void GetInfo() 
+		{
+			Console.WriteLine($"{ Name}");
+			Console.WriteLine($"{ SurName }");
+			Console.WriteLine($"{ Group}");
+			Console.WriteLine($"{ Point}");
+			Console.WriteLine($"{IsGraduate = Point >= 65}");
+			
+		}
+		public void CheckGraduation(string graduation)
+		{
+			if (IsGraduate)
+			{
+				Console.WriteLine("kecib");
+			}
+			else 
+			{
+				Console.WriteLine("kesilib");
+			}
+
+		}
+		public void GetDiplomDegree(string degree)
+		{
+			if(Point>=65 && Point < 80)
+			{
+				Console.WriteLine("adi");
+			}
+			else if( Point>=80 && Point<90) 
+			{
+				Console.WriteLine("sheref");
+			}
+			else
+			{
+				Console.WriteLine("yuksek sheref");
+			}
+		}
+
+
+	}
+}
